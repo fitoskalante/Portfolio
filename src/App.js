d6d30caace1788567300d9e8b01713aabcfa9391
Ricardo Cuevas Escalante <ricardocuevasescalante@Ricardos-MacBook-Pro.local>
2019-11-04 12:28:27 +0700

    monday morning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+
 import Navibar from "./components/Navibar";
 import Landing from "./components/Landing";
 import Projects from "./components/Projects";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footerr from "./components/Footerr";
+
 import "animate.css/animate.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
